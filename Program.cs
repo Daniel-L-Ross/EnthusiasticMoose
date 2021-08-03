d89e559006f865666ad629980c37c4a782ec9a00
Dan Ross <daniellross@gmail.com>
2021-08-03 16:47:21 -0500

    implemented magic moose. refactred to use IsNullOrWhiteSpace

--- Program.cs
+++ Program.cs
@@ -1,73 +1,74 @@
 ﻿using System;
-
+using System.Collections.Generic;
 Main();
 
 void Main()
 {
-    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-    Console.WriteLine("--------------------------------------------");
-    Console.WriteLine();
+    // Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    // Console.WriteLine("--------------------------------------------");
+    // Console.WriteLine();
 
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
+    // CanadaQuestion();
+    // EnthusiasticQuestion();
+    // LoveCSharpQuestion();
+    // SecretQuestion();
+    MagicMoose();
 }
-    void CanadaQuestion()
+void CanadaQuestion()
+{
+    bool isTrue = MooseAsks("Is Canada real");
+    if (isTrue)
     {
-        bool isTrue = MooseAsks("Is Canada real");
-        if (isTrue)
-        {
-            MooseSays("Hmmm... Seems unlikely");
-        }
-        else
-        {
-            MooseSays("I knew IT!!!!");
-        }
+        MooseSays("Hmmm... Seems unlikely");
     }
-
-    void EnthusiasticQuestion()
+    else
     {
-        bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-        if (isEnthusiastic)
-        {
-            MooseSays("Yay!");
-        }
-        else
-        {
-            MooseSays("You should try it!");
-        }
+        MooseSays("I knew IT!!!!");
     }
+}
 
-    void LoveCSharpQuestion()
+void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
     {
-        bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-        if (doesLoveCSharp)
-        {
-            MooseSays("Good job sucking up to your instructor!");
-        }
-        else
-        {
-            MooseSays("You will...oh, yes, you will...");
-        }
+        MooseSays("Yay!");
     }
-
-    void SecretQuestion()
+    else
     {
-        bool wantsSecret = MooseAsks("Do you want to know a secret?");
-        if (wantsSecret)
-        {
-            MooseSays("ME TOO!!!! I love secrets...tell me one!");
-        }
-        else
-        {
-            MooseSays("Oh, no...secrets are the best, I love to share them!");
-        }
+        MooseSays("You should try it!");
     }
+}
 
-    void MooseSays(string message)
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
     {
-        Console.WriteLine($@"
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -95,27 +96,49 @@ void Main()
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
-    }
+}
 
-    bool MooseAsks(string question)
+bool MooseAsks(string question)
+{
+    Console.WriteLine($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
     {
+        Console.WriteLine("Please answer with Y or N only.");
         Console.WriteLine($"{question} (Y/N): ");
-        string answer = Console.ReadLine().ToLower();
-
-        while (answer != "y" && answer != "n")
-        {
-            Console.WriteLine("Please answer with Y or N only.");
-            Console.WriteLine($"{question} (Y/N): ");
-            answer = Console.ReadLine().ToLower();
-        }
-
-        if (answer == "y")
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        answer = Console.ReadLine().ToLower();
     }
 
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MagicMoose()
+{
+    Console.WriteLine("Ask the magic-moose a question... : ");
+    string question = Console.ReadLine();
+    List<string> responses = new List<string>()
+    {
+        "As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+    "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.",
+"Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."
+    };
+
+    bool playing = !(String.IsNullOrWhiteSpace(question));
+    while (playing)
+    {
+        Random randomInt = new Random();
+        int  responsePosition = randomInt.Next(0, (responses.Count - 1));
+        Console.WriteLine($" Magic moose says: {responses[responsePosition]}");
+        Console.WriteLine("Ask the magic-moose another question... : ");
+        question = Console.ReadLine();
+        playing = !(String.IsNullOrWhiteSpace(question));
+    }
+}
