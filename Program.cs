d04ebed6792c8aaf4fed39ecc046e44d557dfb1c
Dan Ross <daniellross@gmail.com>
2021-08-03 11:51:14 -0500

    added moose asks function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 
-namespace EnthusiastecMoose
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+void MooseSays(string message)
 {
-    class Program
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+MooseSays("hello");
+
+bool MooseAsks(string question)
+{
+    Console.WriteLine($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Please answer with Y or N only.");
+        Console.WriteLine($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return  true;
+    }
+    else
+    {
+        return  false;
     }
 }
+
+bool isTrue = MooseAsks("Is Canada real");
+Console.WriteLine(isTrue);
